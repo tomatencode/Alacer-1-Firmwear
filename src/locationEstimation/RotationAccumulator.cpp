23b4f1d3607dfbe37bc0cee8a6c1fa4cfe3a3d29
tomatencode <ingo.thomas@gmx.com>
2026-09-20 10:19:16 +0200

    added roration accumulator

---
+++ src/locationEstimation/RotationAccumulator.cpp
@@ -0,0 +1,72 @@
+#include "RotationAccumulator.hpp"
+
+RotationAccumulator::RotationAccumulator(hardware::IMU& imu, uint32_t maxDt_us)
+    : _imu(imu), _maxDt_us(maxDt_us)
+{}
+
+Eigen::Quaternionf RotationAccumulator::getRotationQuaternion() const {
+    return _rotation;
+}
+
+Eigen::Matrix3f RotationAccumulator::getRotationMatrix() const {
+    return _rotation.toRotationMatrix();
+}
+
+Eigen::Vector3f RotationAccumulator::getEulerAngles_rad() const {
+    return _rotation.toRotationMatrix().eulerAngles(0, 1, 2);
+}
+
+void RotationAccumulator::setRotationQuaternion(const Eigen::Quaternionf& rotation) {
+    _lastUpdate_us = micros(); // accumulation shuld start here
+    _rotation = rotation.normalized();
+}
+
+void RotationAccumulator::setRotationMatrix(const Eigen::Matrix3f& rotationMatrix) {
+    _lastUpdate_us = micros(); // accumulation shuld start here
+    _rotation = Eigen::Quaternionf(rotationMatrix).normalized();
+}
+
+void RotationAccumulator::setEulerAngles_rad(const Eigen::Vector3f& eulerAngles) {
+    _lastUpdate_us = micros(); // accumulation shuld start here
+    _rotation = Eigen::Quaternionf(
+        Eigen::AngleAxisf(eulerAngles[0], Eigen::Vector3f::UnitX()) *
+        Eigen::AngleAxisf(eulerAngles[1], Eigen::Vector3f::UnitY()) *
+        Eigen::AngleAxisf(eulerAngles[2], Eigen::Vector3f::UnitZ())
+    ).normalized();
+}
+
+
+Eigen::Vector3f RotationAccumulator::getAngularVelocity_rad_s() const {
+    return _angularVelocity_rad_s;
+}
+
+void RotationAccumulator::update() {
+    const uint32_t currentTime_us = micros();
+
+    auto gyro = _imu.getGyro();
+    
+    _angularVelocity_rad_s = {
+        gyro.x_rad_s,
+        gyro.y_rad_s,
+        gyro.z_rad_s    
+    };
+
+
+    if (_lastUpdate_us == 0) { // First update, initialize the last update time
+        _lastUpdate_us = currentTime_us;
+        return;
+    }
+
+    const uint32_t measuredDt_us = currentTime_us - _lastUpdate_us;
+    _lastUpdate_us = currentTime_us;
+
+    const float dt = min(measuredDt_us, _maxDt_us) * 1e-6f;
+
+    const Eigen::Vector3f deltaTheta = _angularVelocity_rad_s * dt;
+    const float angle = deltaTheta.norm();
+
+    if (angle > 1e-6f) {
+        const Eigen::Quaternionf deltaRotation(Eigen::AngleAxisf(angle, deltaTheta / angle));
+        _rotation = (_rotation * deltaRotation).normalized();
+    }
+}
---
+++ src/locationEstimation/RotationAccumulator.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <ArduinoEigen.h>
+
+#include "../hardware/imu/IMU.hpp"
+
+class RotationAccumulator {
+public:
+    RotationAccumulator(hardware::IMU& imu, uint32_t maxDt_us);
+
+    Eigen::Quaternionf getRotationQuaternion() const;
+    Eigen::Matrix3f getRotationMatrix() const;
+    Eigen::Vector3f getEulerAngles_rad() const;
+
+    void setRotationQuaternion(const Eigen::Quaternionf& rotation);
+    void setRotationMatrix(const Eigen::Matrix3f& rotationMatrix);
+    void setEulerAngles_rad(const Eigen::Vector3f& eulerAngles);
+
+    Eigen::Vector3f getAngularVelocity_rad_s() const;
+
+    void update();
+
+private:
+    hardware::IMU& _imu;
+
+    uint32_t _lastUpdate_us = 0;
+
+    uint32_t _maxDt_us;
+
+    Eigen::Quaternionf _rotation = Eigen::Quaternionf::Identity();
+    Eigen::Vector3f _angularVelocity_rad_s = Eigen::Vector3f::Zero();
+};
